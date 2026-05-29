6cf3cf3954a4a73c2f269d5fe7f613c0a37e988b
rahul paladugu <rahulpaladugu96@gmail.com>
2026-05-30 11:11:49 +1200

    terraform-infrastructure

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 component=$1
-dnf install ansible -y
+sudo dnf install ansible -y
 ansible-pull -U https://github.com/rahul-paladugu/roboshop-ansible-collection.git -e component=$component main.yaml
