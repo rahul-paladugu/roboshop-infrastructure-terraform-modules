255d8dc26f834420095b889cf0cb08a1176739fd
rahul paladugu <rahulpaladugu96@gmail.com>
2026-06-03 13:08:44 +1200

    Change output key from 'id' to 'instance_id'

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -14,12 +14,12 @@ resource "null_resource" "wait_for_mongodb" {
   depends_on = [module.mongodb_server]
 
   provisioner "local-exec" {
-    command = "aws ec2 wait instance-status-ok --instance-ids ${module.mongodb_server.id}"
+    command = "aws ec2 wait instance-status-ok --instance-ids ${module.mongodb_server.instance_id}"
   }
 }
 #Configure mongodb
 resource "terraform_data" "mongodb_setup" {
-  triggers_replace = [module.mongodb_server.id]
+  triggers_replace = [module.mongodb_server.instance_id]
   connection {
     host = module.mongodb_server.private_ip
     user = local.remote_user
@@ -53,12 +53,12 @@ resource "null_resource" "wait_for_redis" {
   depends_on = [module.redis_server]
 
   provisioner "local-exec" {
-    command = "aws ec2 wait instance-status-ok --instance-ids ${module.redis_server.id}"
+    command = "aws ec2 wait instance-status-ok --instance-ids ${module.redis_server.instance_id}"
   }
 }
 #Configure redis
 resource "terraform_data" "redis_setup" {
-  triggers_replace = [module.redis_server.id]
+  triggers_replace = [module.redis_server.instance_id]
   connection {
     host = module.redis_server.private_ip
     user = local.remote_user
@@ -94,12 +94,12 @@ resource "null_resource" "wait_for_rabbitmq" {
   depends_on = [module.rabbitmq_server]
 
   provisioner "local-exec" {
-    command = "aws ec2 wait instance-status-ok --instance-ids ${module.rabbitmq_server.id}"
+    command = "aws ec2 wait instance-status-ok --instance-ids ${module.rabbitmq_server.instance_id}"
   }
 }
 #Configure rabbitmq
 resource "terraform_data" "rabbitmq_setup" {
-  triggers_replace = [module.rabbitmq_server.id]
+  triggers_replace = [module.rabbitmq_server.instance_id]
   connection {
     host = module.rabbitmq_server.private_ip
     user = local.remote_user
--- 40-databases/outputs.tf
+++ 40-databases/outputs.tf
@@ -1,11 +1,11 @@
 output "mongodb_instance_id" {
-  value = module.mongodb_server.id
+  value = module.mongodb_server.instance_id
 }
 
 output "redis_instance_id" {
-  value = module.redis_server.id
+  value = module.redis_server.instance_id
 }
 
 output "rabbitmq_instance_id" {
-  value = module.rabbitmq_server.id
+  value = module.rabbitmq_server.redis_instance_id
 }
