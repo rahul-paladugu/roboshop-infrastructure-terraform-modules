e9e56f942dc7fe352e4075e06992ce75d03a7c28
rahul paladugu <rahulpaladugu96@gmail.com>
2026-06-04 14:44:11 +1200

    infra-setup

--- 40-databases/local.tf
+++ 40-databases/local.tf
@@ -4,6 +4,7 @@ locals {
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
     remote_user =  data.aws_ssm_parameter.remote_user.value
     remote_user_password = data.aws_ssm_parameter.remote_user_password.value
 }
