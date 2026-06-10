8c43fc112e2093e0d4927532c12113fcc6047d89
rahul paladugu <rahulpaladugu96@gmail.com>
2026-06-10 14:40:53 +1200

    updated outputs

--- 40-databases/outputs.tf
+++ 40-databases/outputs.tf
@@ -9,3 +9,7 @@ output "redis_instance_id" {
 output "rabbitmq_instance_id" {
   value = module.rabbitmq_server.instance_id[0]
 }
+
+output "mysql_instance_id" {
+  value = module.mysql_server.instance_id[0]
+}
