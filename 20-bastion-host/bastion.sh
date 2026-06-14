4c10000844d877213d3e8fe7c4de831fffbf7878
rahul paladugu <rahulpaladugu96@gmail.com>
2026-06-14 12:03:06 +1200

    updated shell script to clone repositry

--- 20-bastion-host/bastion.sh
+++ 20-bastion-host/bastion.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Growing the /home volume for terraform purpose
+growpart /dev/nvme0n1 4
+lvextend -L +25G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
+
+
+#Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+#Clone Repositry
+mkdir -p /home/ec2-user/git-repos
+cd /home/ec2-user/git-repos
+git clone https://github.com/rahul-paladugu/roboshop-infrastructure-terraform-modules
