fd29a7720c6d1b5ae71db9b4c3b1010e5a644d7a
rahul paladugu <rahulpaladugu96@gmail.com>
2026-06-14 16:42:38 +1200

    catalogue-config

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -98,8 +98,8 @@ resource "aws_autoscaling_group" "catalogue" {
   dynamic "tag" {
   for_each =  local.asg_tags  
     content {
-    key                 = each.key
-    value               = each.Value
+    key                 = tag.key
+    value               = tag.Value
     propagate_at_launch = true
   }
   }
